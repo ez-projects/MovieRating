84814ffa8f69922084203ec7402155499f97e730
En Zhou <zhouen.nathan@gmail.com>
2017-06-07 23:03:52 +0200

    Formatted logs

--- movie_rating/movie_rating.py
+++ movie_rating/movie_rating.py
@@ -20,7 +20,9 @@ handler = logging.FileHandler('./log/movie_rating.log')
 handler.setLevel(logging.INFO)
 
 # create a logging format
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter(
+    '[%(asctime)s] p%(process)s {%(pathname)s:%(lineno)d [%(funcName)s]} %(levelname)s - %(message)s','%m-%d %H:%M:%S'
+)
 handler.setFormatter(formatter)
 
 # add the handlers to the logger
